dce592b9655625c67c43d378bae9c365b097ef06
zxiong1@yahoo.com <zxiong1@yahoo.com>
2021-08-30 09:31:28 -0400

    init checkin

---
+++ familyT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  familyT
+//
+//  Created by Xiong, Zach (Z.) on 8/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world from Xiong's!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ familyT/familyTApp.swift
@@ -0,0 +1,17 @@
+//
+//  familyTApp.swift
+//  familyT
+//
+//  Created by Xiong, Zach (Z.) on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct familyTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
